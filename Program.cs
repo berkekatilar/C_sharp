c55230a1ccc80f4555b69ac16b68c90180ee83d0
[Berke Katılar] <[berkekatilar@gmail.com]>
2022-03-06 18:59:50 +0300

    operator

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿int x = 5;
+int y = 10;
+int sonuc;
+y = y+5;
+
+y += 2;
+x *= 2;
+x /= 1 ;
+//aritmetik işlemler
+sonuc = x+y;
+Console.WriteLine(sonuc);
+
+sonuc = x-y;
+Console.WriteLine(sonuc);
+
+sonuc = x*y;
+Console.WriteLine(sonuc);
+
+sonuc = x/y;
+Console.WriteLine(sonuc);
+
+sonuc = x%3; // mod alma
+Console.WriteLine(sonuc);
+
+//mantıksal işlemler
+bool issuc = true;
+bool isntsuc = false;
+
+if (issuc && isntsuc)
+{
+Console.WriteLine("complated");
+}
+if (issuc||isntsuc)
+{
+Console.WriteLine("not complated");
+}
+if (issuc&&!isntsuc)
+{
+Console.WriteLine("nice");
+}
+
+//ilişkisel işlemler
+bool sonuc1;
+
+sonuc1 = x<=y;
+Console.WriteLine(sonuc);
+
+sonuc1 = x>=y;
+Console.WriteLine(sonuc);
+
+sonuc1 = x==y;
+Console.WriteLine(sonuc);
+
+sonuc1 = x!=y;
+Console.WriteLine(sonuc);
