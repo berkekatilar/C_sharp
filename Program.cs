0e9b71509ac54fdefcb44f4e3b0bcc87bb2b1c4f
[Berke Katılar] <[berkekatilar@gmail.com]>
2022-03-30 20:59:27 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿ class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geridonus_tipi metot_adi(parametreListesi/arguman)
+            // {
+            //     komutlar;
+            //      return;
+            // }
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a, b);
+            System.Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirveTopla(ref a, ref b);
+            System.Console.WriteLine(Convert.ToString(sonuc2));
+            System.Console.WriteLine(Convert.ToString(a + b));
+            Console.Read();
+
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            System.Console.WriteLine(veri);
+        }
+
+        public int ArttirveTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
