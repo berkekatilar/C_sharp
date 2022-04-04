f1f7a8d60ef6eecddd7b72f05bc7bdd59d82c989
[Berke Katılar] <[berkekatilar@gmail.com]>
2022-04-04 22:22:41 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Bir cümle giriniz: ");
+            string cumle = Convert.ToString(Console.ReadLine());
+
+            string[] parcalanmısCumle = cumle.Split(" ");
+            char[] harf = cumle.ToCharArray();
+            int ktoplam = 0;
+            int htoplam = 0;
+
+
+            foreach (var item in parcalanmısCumle)
+            {
+                ktoplam++;
+            }
+
+            foreach (var item in harf)
+            {
+                if(item == ' ')
+                {
+                    continue;
+                }
+                htoplam++;
+            }
+
+            Console.WriteLine("Kelime sayısı: " +ktoplam);
+            Console.WriteLine("Harf Toplamı: " +htoplam);
